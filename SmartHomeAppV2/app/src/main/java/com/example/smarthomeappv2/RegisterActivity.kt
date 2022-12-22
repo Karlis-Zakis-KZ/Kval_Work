ee79b01c75818ceb7f061a02c636dd4f65307480
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2022-12-22 23:40:07 +0200

    SmartHomeAppV2

---
+++ SmartHomeAppV2/app/src/main/java/com/example/smarthomeappv2/RegisterActivity.kt
@@ -0,0 +1,19 @@
+package com.example.smarthomeappv2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val buttonClick = findViewById<Button>(R.id.backToLogin)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
