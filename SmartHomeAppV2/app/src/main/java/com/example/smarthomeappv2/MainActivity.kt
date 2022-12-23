61c12a70669459bde1b2742b43b085b8fcc351d3
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2022-12-23 22:54:03 +0200

    Adding navigation Bar

--- SmartHomeAppV2/app/src/main/java/com/example/smarthomeappv2/MainActivity.kt
+++ SmartHomeAppV2/app/src/main/java/com/example/smarthomeappv2/MainActivity.kt
@@ -4,16 +4,49 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.smarthomeappv2.databinding.ActivityLoginBinding
+import com.example.smarthomeappv2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNavigationBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.Pricing -> replaceFragment(PricingFragment())
+                R.id.User -> replaceFragment(UserFragment())
+                else -> {
+                }
+            }
+            true
+        }
         val buttonClick = findViewById<Button>(R.id.mainPageSignUp)
         buttonClick.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
     }
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+
+        fragmentTransition.replace(R.id.fragment_container,fragment)
+        fragmentTransition.commit()
+
+
+    }
+
 }
